afafd65f0094564e9d38650a2860ed0f76833650
Q456lm <cbbunting1@my.waketech.edu>
2025-04-15 13:51:30 -0400

    They were spies, spies I tell you, I went to Harvard and I was the smartest one there and they were spies.

--- classes-hw.js
+++ classes-hw.js
@@ -20,7 +20,17 @@ const problems = [
     //  - subtract(x, y): returns the result of x minus y
     // Then create an instance and call both methods.
 
-    // Write your class and code here
+    class Calculator{
+      add(x,y){
+        return x+y;
+      }
+      subtract(x,y){
+        return x-y;
+      }
+    }
+
+    let calc = new Calculator();
+    output.innerHTML = (`Add: ${calc.add(9,3)} <div>Subtract: ${calc.subtract(27,12)}</div> `);
   },
   function problem2() {
     // Problem 2:
@@ -29,15 +39,39 @@ const problems = [
     //  - getName(): returns the name
     // Then create an instance, set the name to your name, and log it using getName().
 
-    // Write your class and code here
+    class Person{
+      setName(name){
+        this.name = name;
+      }
+      getName(){
+        return this.name;
+      }
+    }
+
+    let hum = new Person()
+    hum.setName("Steve")
+    output.innerHTML = (`I am ${hum.getName()}`);
   },
   function problem3() {
     // Problem 3:
     // Create a class called Bird with a method fly() that logs "Flying"
     // Create a class called Penguin that extends Bird and overrides the fly() method to log "Penguins can't fly"
     // Then create one Bird and one Penguin, and call their fly() methods.
+    class Bird{
+      fly(){
+        return "Flying";
+      }
+    }
 
-    // Write your class and code here
+    class Penguin extends Bird{
+      fly(){
+        return "Penguins can't fly";
+      }
+    }
+    
+    let bierd = new Bird();
+    let pengin = new Penguin();
+    output.innerHTML = (`${bierd.fly()}<div>${pengin.fly()}</div>`)
   },
   function problem4() {
     // Problem 4:
@@ -47,7 +81,27 @@ const problems = [
     // - Square's describe() method logs "I'm a square"
     // Then create instances of Circle and Square and call their describe() methods.
 
-    // Write your class and code here
+    class Shape{
+      describe(){
+        return "I am shape";
+      }
+    }
+
+    class Circle extends Shape{
+      describe(){
+        return "I am circle";
+      }
+    }
+
+    class Square extends Shape{
+      describe(){
+        return "I am square";
+      }
+    }
+    let cicle = new Circle();
+    let scare = new Square();
+    output.innerHTML = (`${cicle.describe()}<div>and ${scare.describe()}</div>`)
+
   },
   function problem5() {
     // Problem 5:
@@ -55,7 +109,20 @@ const problems = [
     // Create a subclass called Manager that has an additional method hireEmployee() that logs "Employee hired"
     // Then create an instance of Manager, and call both methods.
 
-    // Write your class and code here
+    class Employee{
+      work(){
+        return "Working...";
+      }
+    }
+
+    class Manager extends Employee{
+      hireEmployee(){
+        return "Employee hired";
+      }
+    }
+
+    let theman = new Manager();
+    output.innerHTML = (`${theman.work()}<div>${theman.hireEmployee()}</div>`);
   }
 ];
 
